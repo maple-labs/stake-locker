a07735f3f1ebcb86cfedf59497e23a722d3f8e90
Michael De Luca <michael.deluca@circle-free.com>
2021-08-10 17:37:44 -0400

    fix: PR Review 2

--- contracts/test/StakeLocker.t.sol
+++ contracts/test/StakeLocker.t.sol
@@ -89,7 +89,7 @@ contract StakeLockerStakerTest is DSTest {
     }
 
     function constrictToRange(uint256 value, uint256 min, uint256 max) internal pure returns (uint256) {
-        return value % (max - min) + min;        
+        return (value % (max - min)) + min;
     }
 
     function setUp() external {
@@ -146,9 +146,10 @@ contract StakeLockerStakerTest is DSTest {
         assertEq(locker.unstakeCooldown(address(staker)), 0);
     }
 
-    function test_unstake(uint256 stakeAmount, uint256 unstakeAmount) external {
-        stakeAmount = constrictToRange(stakeAmount, 10, type(uint256).max / block.timestamp);
-        unstakeAmount = constrictToRange(unstakeAmount, 10, stakeAmount);
+    function test_unstake(uint256 stakeAmount, uint256 unstakeAmount1, uint256 unstakeAmount2) external {
+        stakeAmount    = constrictToRange(stakeAmount, 100, type(uint256).max / block.timestamp);
+        unstakeAmount1 = constrictToRange(unstakeAmount1, stakeAmount / 4, stakeAmount / 2);
+        unstakeAmount2 = constrictToRange(unstakeAmount2, stakeAmount / 4, stakeAmount / 2);
 
         stakeToken.mint(address(staker), stakeAmount);
         staker.erc20_approve(address(stakeToken), address(locker), stakeAmount);
@@ -161,33 +162,40 @@ contract StakeLockerStakerTest is DSTest {
         hevm.warp(start + globals.stakerCooldownPeriod() + (globals.stakerUnstakeWindow() / 2));
 
         // Can't unstake in unstake window yet (lockup not expired)
-        assertTrue(!staker.try_stakeLocker_unstake(address(locker), unstakeAmount));
+        assertTrue(!staker.try_stakeLocker_unstake(address(locker), 1));
 
         staker.stakeLocker_cancelUnstake(address(locker));
 
-        hevm.warp(start + locker.lockupPeriod() + 1);
+        hevm.warp(start + locker.lockupPeriod());
 
         // Can't unstake immediately (did not intend)
-        assertTrue(!staker.try_stakeLocker_unstake(address(locker), unstakeAmount));
+        assertTrue(!staker.try_stakeLocker_unstake(address(locker), 1));
 
         staker.stakeLocker_intendToUnstake(address(locker));
 
-        hevm.warp(start + locker.lockupPeriod() + 1 + globals.stakerCooldownPeriod() - 1);
+        hevm.warp(start + locker.lockupPeriod() + globals.stakerCooldownPeriod() - 1);
 
         // Can't unstake in cooldown
-        assertTrue(!staker.try_stakeLocker_unstake(address(locker), unstakeAmount));
+        assertTrue(!staker.try_stakeLocker_unstake(address(locker), 1));
 
-        hevm.warp(start + locker.lockupPeriod() + 1 + globals.stakerCooldownPeriod());
+        hevm.warp(start + locker.lockupPeriod() + globals.stakerCooldownPeriod());
 
-        // Can unstake in unstake window
-        assertTrue(staker.try_stakeLocker_unstake(address(locker), unstakeAmount / 2));
+        // Can unstake at start of unstake window
+        assertTrue(staker.try_stakeLocker_unstake(address(locker), unstakeAmount1));
         
-        assertEq(locker.balanceOf(address(staker)), stakeAmount - (unstakeAmount / 2));
+        assertEq(locker.balanceOf(address(staker)), stakeAmount - unstakeAmount1);
 
-        hevm.warp(start + locker.lockupPeriod() + 2 + globals.stakerCooldownPeriod() + globals.stakerUnstakeWindow());
+        hevm.warp(start + locker.lockupPeriod() + globals.stakerCooldownPeriod() + globals.stakerUnstakeWindow());
+
+        // Can unstake at end of unstake window
+        assertTrue(staker.try_stakeLocker_unstake(address(locker), unstakeAmount2));
+        
+        assertEq(locker.balanceOf(address(staker)), stakeAmount - (unstakeAmount1 + unstakeAmount2));
+
+        hevm.warp(start + locker.lockupPeriod() + globals.stakerCooldownPeriod() + globals.stakerUnstakeWindow() + 1);
 
         // Can't unstake past unstake window
-        assertTrue(!staker.try_stakeLocker_unstake(address(locker), unstakeAmount / 2));
+        assertTrue(!staker.try_stakeLocker_unstake(address(locker), 1));
     }
 
 }
